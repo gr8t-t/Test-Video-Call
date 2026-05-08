fa4692aef17e395c2575f24ef69eb3578f651ea4
gr8t-t <mavtechhub@gmail.com>
2026-05-08 16:37:28 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,4 +7,5 @@
 export const AUTHORIZED_DEVICES = [
   { email: "john@gmail.com",  password: "MARV-X7K2-P9QR", label: "John's Laptop" },
   { email: "sarah@gmail.com", password: "MARV-B3T8-W1NX", label: "Sarah's PC"    },
+  { email: "joe@gmail.com",  password: "091303708", label: "John's Laptop" },
 ];
