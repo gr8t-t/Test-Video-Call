e15cec3be280148a0c70016576723ddd714c820e
gr8t-t <mavtechhub@gmail.com>
2026-05-16 12:07:47 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,10 +8,8 @@ export const AUTHORIZED_DEVICES = [
   { email: "john@gmail.com",  password: "MARV-X7K2-P9QR", label: "John's Laptop" },
   { email: "sarah@gmail.com", password: "MARV-B3T8-W1NX", label: "Sarah's PC"    },
   { email: "joe@gmail.com",  password: "091303708", label: "John's Laptop" },
-  { email: "gerrymolloy087@gmail.com",  password: "MARV-B3T8-W1NO", label: "John's Laptop" },
   { email: "andyjason87@gmail.com",  password: "MARV-B3T8-X1N2", label: "John's Laptop" },
   { email: "joy438704@gmail.com",  password: "MARV-B3T8-X1T3", label: "John's Laptop" },
   { email: "michaelolowookere25@gmail.com",  password: "MARV-B669-X1T3", label: "John's Laptop" },
-  { email: "iselepraise33@gmail.com", password: "MARV-B3T8-W1TT", label: "SANGWAI PC"    },
   { email: "sangwai12123@gmail.com", password: "MARV-B3T8-W1NZ", label: "SANGWAI PC"    },
 ];
